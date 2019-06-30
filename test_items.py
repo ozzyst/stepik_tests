c8a27bb60c0a76753ab53b228a4a4097d5638394
s.losev <s.losev@yadro.com>
2019-06-30 19:09:37 +0300

    fixed link

--- test_items.py
+++ test_items.py
@@ -1,4 +1,8 @@
+import time
+
+
 def test_items(browser):
-    browser.get('http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/')
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     button = browser.find_element_by_class_name('btn-add-to-basket')
+    #time.sleep(100)
     assert button, 'button not found!'
